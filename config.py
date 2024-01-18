e002334c2666c228e72163ee081fc2219a8e2456
Polina Vishnyakova <vishnyakovapv@gmail.com>
2024-01-18 17:07:00 +0400

    delete .env and add to .gitignore

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ from utils.utils import abs_path_from_project
 
 
 class Config(BaseSettings):
-    contex: Literal["bstack", "selenoid", "local"] = "local"
     browser_version: str = "100"
     LOGIN: str = "vishpolina796@gmail.com"
     PASSWORD: str = "Polina21@"
@@ -17,5 +16,5 @@ class Config(BaseSettings):
     password_selenoid: str = "1234"
 
 
-dotenv.load_dotenv(dotenv_path=abs_path_from_project(f'.env.{Config().contex}'))
-settings = Config(_env_file=abs_path_from_project(f'.env.{Config().contex}'))
+dotenv.load_dotenv(dotenv_path=abs_path_from_project('.env.'))
+settings = Config(_env_file=abs_path_from_project('.env'))
